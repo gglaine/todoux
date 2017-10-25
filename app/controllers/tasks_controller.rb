e1af882438c7e2c6a7f76bd7e54083a29f0915ee
Guillaume Laine <guillaumelaine2.0@gmail.com>
2017-10-25 11:27:42 +0200

    styled new task form - rendered in partial in index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order(created_at: :desc)
   end
 
   def new
@@ -10,10 +10,17 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+    redirect_to tasks_path
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
