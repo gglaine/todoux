a734fb8be7d4754d4f820c38ca0665859d29c27e
Guillaume Laine <guillaumelaine2.0@gmail.com>
2017-11-11 19:01:38 +0100

    design and helpers for percentage completed

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -4,4 +4,29 @@ module TasksHelper
     @tasks.select { |task| task["done"] }
   end
 
+  def react_to_workload
+    count = @tasks.count
+
+    cool =  "Cool raoul !"
+    chill = "I think you should Chill Bill !"
+    naptime = "Et si on faisait une sieste"
+
+    case count
+      when 0..5
+        return  cool
+      when 6..10
+        return chill
+      when 11..15
+        return nap
+    end
+  end
+
+  def completed_tasks_percentage
+    tasks = @tasks.count.to_f
+    complete = done_tasks.count.to_f
+    percentage = (complete / tasks) * 100
+    # percentage = percentage / 100
+    return percentage.to_i
+  end
+
 end
